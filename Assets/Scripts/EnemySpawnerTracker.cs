5fc0fc628f400e4d33b6b496755d023cd31bae6a
Justin-Ong <justinong32@gmail.com>
2021-04-07 15:32:46 +0800

    Make enemy spawner highlight system scalable Make enemy spawner highlight UI icons appear and disappear as they should instead of staying on screen when not needed

--- Assets/Scripts/EnemySpawnerTracker.cs
+++ Assets/Scripts/EnemySpawnerTracker.cs
@@ -5,30 +5,30 @@ using UnityEngine.UI;
 
 public class EnemySpawnerTracker : MonoBehaviour
 {
-    public Image OnScreenSprite; // can use gameobject with spriterenderer, or reference a Sprite here, and create a new gameobject, add a sprite renderer to it.
+    public Image OnScreenSprite;
     public Image OffScreenSprite;
     public List<GameObject> objects;
-    //public Image[] onScreenSprites; // sprite object pooling
-    //public Image[] offScreenSprites;
 
-    public Vector3 objectPoolPos;
-    Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) * .5f;
+    //public Vector3 objectPoolPos;
+    Image[] onScreenSprites;
+    Image[] offScreenSprites;
+    //Vector3 screenCenter = new Vector3(Screen.width, Screen.height, 0) * .5f;
 
-    // Use this for initialization
     void Start()
     {
-        //onScreenSprites = new GameObject[objects.Count];
-        //offScreenSprites = new GameObject[objects.Count];
-        //Debug.Log ("Center: " + screenCenter);
-        //create the sprites at load time
+        onScreenSprites = new Image[objects.Count];
+        offScreenSprites = new Image[objects.Count];
         for (int i = 0; i < objects.Count; i++)
         {
-            //onScreenSprites[i] = Instantiate(OnScreenSprite,objectPoolPos,this.transform.rotation) as GameObject;
-            //offScreenSprites[i] = Instantiate(OffScreenSprite,objectPoolPos,this.transform.rotation) as GameObject;
+            Image newOnScreenSprite = Instantiate(OnScreenSprite, transform.position, transform.rotation);
+            Image newOffScreenSprite = Instantiate(OffScreenSprite, transform.position, transform.rotation);
+            newOnScreenSprite.transform.SetParent(gameObject.transform);
+            newOffScreenSprite.transform.SetParent(gameObject.transform);
+            onScreenSprites[i] = newOnScreenSprite;
+            offScreenSprites[i] = newOffScreenSprite;
         }
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         PlaceIndicators();
@@ -36,29 +36,27 @@ public class EnemySpawnerTracker : MonoBehaviour
 
     void PlaceIndicators()
     {
-        //GameObject[] objects = GameObject.FindObjectsOfType(typeof(AI)) as GameObject[]; //find objects by type (might ahve to find by gameobejct, and then filter for AI)
-        //go through all the objects we care about
-        foreach (GameObject go in objects)
+        foreach (GameObject obj in objects)
         {
-            Vector3 screenpos = Camera.main.WorldToScreenPoint(go.transform.position);
-
-            //if onscreen
+            Vector3 screenpos = Camera.main.WorldToScreenPoint(obj.transform.position);
+            int index = objects.IndexOf(obj);
             if (screenpos.z > 0 && screenpos.x < Screen.width && screenpos.x > 0 && screenpos.y < Screen.height && screenpos.y > 0)
             {
-
-                int index = objects.IndexOf(go); // get an index to use for our sprite, based on the objects index in the objects list // we should make an objectpool manager
-                OnScreenSprite.rectTransform.position = screenpos;
-                //Debug.Log("OnScreen: " + screenpos);
+                onScreenSprites[index].rectTransform.position = screenpos;
+                onScreenSprites[index].enabled = true;
+                offScreenSprites[index].enabled = false;
             }
             else
             {
-                PlaceOffscreen(screenpos);
+                PlaceOffscreen(screenpos, offScreenSprites[index]);
+                onScreenSprites[index].enabled = false;
+                offScreenSprites[index].enabled = true;
             }
         }
 
     }
 
-    void PlaceOffscreen(Vector3 screenpos)
+    void PlaceOffscreen(Vector3 screenpos, Image sprite)
     {
         float x = screenpos.x;
         float y = screenpos.y;
@@ -88,7 +86,6 @@ public class EnemySpawnerTracker : MonoBehaviour
             y = offset;
         }
 
-        OffScreenSprite.rectTransform.position = new Vector3(x, y, 0);
-
+        sprite.rectTransform.position = new Vector3(x, y, 0);
     }
 }
